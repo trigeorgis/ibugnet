30377d0d4606455bfd75bd81d4b19952530621fc
George Trigeorgis <trigeorgis@gmail.com>
2016-08-06 12:05:47 +0100

    add doc in l1 loss

--- losses.py
+++ losses.py
@@ -1,8 +1,22 @@
 import tensorflow as tf
 
+
 def smooth_l1(pred, ground_truth):
-    residual = tf.abs(pred - ground_truth)
-    loss = tf.select(tf.less(residual, 1),
-                     0.5 * tf.square(residual),
-                     residual - .5)
+    '''Calculates the smoothed l1 loss.
+
+    The prediction and ground truth should be l1 normalised.
+
+    Args:
+      pred: A tf Tensor of dimensions [num_images, height, width, 3].
+      ground_truth: A tf Tensor of dimensions [num_images, height, width, 3].
+    Returns:
+      A scalar with the mean loss.
+    '''
+
+    absolute_residual = tf.abs(pred - ground_truth)
+
+    loss = tf.select(
+        tf.less(absolute_residual, 1), 0.5 * tf.square(absolute_residual),
+        absolute_residual - .5)
+
     return tf.reduce_mean(loss, name='smooth_l1')
