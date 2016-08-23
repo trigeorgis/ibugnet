c4de71cc45d3dc6da25ae5c2ba95277f6a3d7494
trigeorgis <trigeorgis@gmail.com>
2016-08-23 10:58:52 +0300

    Update losses.py
    Add a doc string to the smooth_l1 function.

--- losses.py
+++ losses.py
@@ -1,6 +1,18 @@
 import tensorflow as tf
 
 def smooth_l1(pred, ground_truth):
+    """Defines a robust L1 loss.
+
+    This is a robust L1 loss that is less sensitive to outliers
+    than the traditional L2 loss. This was defined in Ross
+    Girshick's Fast R-CNN, ICCV 2015 paper.
+    
+    Args:
+      pred: a Tensor with the predictions.
+      ground_truth: a Tensor with the ground truth labels.
+    Returns:
+      a scalar defining the smoothed l1 loss.
+    """
     residual = tf.abs(pred - ground_truth)
     loss = tf.select(tf.less(residual, 1),
                      0.5 * tf.square(residual),
